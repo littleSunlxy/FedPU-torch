21128e3840848048099de3dd141c32ceb053d44b
linxinyang <linxinyang@meituan.com>
2021-05-24 11:34:34 +0800

    config setting

--- options.py
+++ options.py
@@ -4,29 +4,29 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--pu_lr', type=float, default=0.1)
 parser.add_argument('--pu_weight_decay', type=float, default=5e-3)
-parser.add_argument('--pu_batchsize', type=int, default=4096)
+parser.add_argument('--pu_batchsize', type=int, default=2048)
 parser.add_argument('--momentum', type=float, default=0.5)
 parser.add_argument('--dataset', type=str, default='MNIST')
 #parser.add_argument('--dataset', type=str, default='CIFAR10')
 parser.add_argument('--num_classes', type=int, default=10)
-#parser.add_argument('--label_dir', type=str, default='/workdir/lxy/experiment/data/cifar10/')
-parser.add_argument('--label_dir', type=str, default='/workdir/lxy/experiment/data/mnist/')
+parser.add_argument('--label_dir', type=str, default='/workdir/lxy/experiment/data/cifar10/')
+#parser.add_argument('--label_dir', type=str, default='/workdir/lxy/experiment/data/mnist/')
 
 # pu on clients
 parser.add_argument('--pu_weight', type=float, default=1)#1
-parser.add_argument('--local_epochs', type=int, default=1)
+parser.add_argument('--local_epochs', type=int, default=10)
 #parser.add_argument('--positiveIndex', type=str, default='k')  # 第k类为负类
 parser.add_argument('--positiveIndex', type=str, default='randomIndexList') #随机选两个标签为负类
-parser.add_argument('--positiveRate', type=float, default=0.99)#1
+parser.add_argument('--positiveRate', type=float, default=1/3)#1
 parser.add_argument('--randomIndex_num', type=int, default=5)
 
 # FL aggregator
-parser.add_argument('--num_clients', type=int, default=10)
-parser.add_argument('--communication_rounds', type=int, default=2000)
-parser.add_argument('--classes_per_client', type=int, default=5)
+parser.add_argument('--num_clients', type=int, default=4)
+parser.add_argument('--communication_rounds', type=int, default=400)
+parser.add_argument('--classes_per_client', type=int, default=6)
 #parser.add_argument('--participating_ratio', type=float, default=0.7)
 parser.add_argument('--clientSelect_Rate', type=float, default=0.5)
-parser.add_argument('--imagename', type=str, default='5.23.1.jpg')
+parser.add_argument('--imagename', type=str, default='5.23.3.jpg')
 
 opt, _ = parser.parse_known_args()
 
