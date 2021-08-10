56245897d87f37a1ba68d9badb65d734697f8ea4
linxinyang <linxinyang@meituan.com>
2021-08-10 22:33:04 +0800

    config setting

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 
 # client 100 contrast
-python main.py  --positiveRate 0.01 --randomIndex_num 2 --num_clients 10 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.1
+python main.py  --positiveRate 0.01 --randomIndex_num 2 --num_clients 15 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.1
