f60499e33eb1114f826ee295a2fa3a60a15268f8
linxinyang <linxinyang@meituan.com>
2021-08-10 22:27:02 +0800

    config setting

---
+++ train20.sh
@@ -0,0 +1,8 @@
+# client 100 contrast
+#python main.py  --dataset 'CIFAR10' --positiveRate 0.01 --randomIndex_num 2 --num_clients 100 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.05
+# client 100 contrast
+#python main.py  --positiveRate 0.01 --randomIndex_num 2 --num_clients 100 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.05
+# client 50
+#python main.py  --positiveRate 0.01 --randomIndex_num 2 --num_clients 100 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.05
+# client 20
+python main.py  --positiveRate 0.01 --randomIndex_num 2 --num_clients 100 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.05
---
+++ train_contrast100.sh
@@ -0,0 +1,2 @@
+# client 100 contrast
+python main.py  --dataset 'CIFAR10' --positiveRate 0.01 --randomIndex_num 2 --num_clients 100 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.05
