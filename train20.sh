e21f0c0784274f7c11a62338ae67df125ae3cb62
linxinyang <linxinyang@meituan.com>
2021-08-11 19:22:11 +0800

    config setting

--- train20.sh
+++ train20.sh
@@ -7,6 +7,6 @@
 #python main.py  --positiveRate 0.33 --positiveIndex '0' --P_Index_accordance --local_epochs 2 --randomIndex_num 2 --num_clients 20 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
 
 # baseline 2
-python main.py  --positiveRate 0.999 --positiveIndex '0' --P_Index_accordance --local_epochs 2 --randomIndex_num 2 --num_clients 20 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
+python main.py  --positiveRate 0.999 --positiveIndex '0' --P_Index_accordance --local_epochs 2 --randomIndex_num 10 --num_clients 20 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
 
 
