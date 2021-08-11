d3ac08a3c772b0947ba3210439499f22b3e85d8f
linxinyang <linxinyang@meituan.com>
2021-08-11 19:20:31 +0800

    config setting

--- train50.sh
+++ train50.sh
@@ -2,9 +2,9 @@
 #python main.py  --positiveRate 0.33  --local_epochs 3 --P_Index_accordance --randomIndex_num 2 --num_clients 50 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
 
 # baseline 1
-python main.py  --positiveRate 0.33  --positiveIndex '0' --local_epochs 3 --P_Index_accordance --randomIndex_num 2 --num_clients 50 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
+#python main.py  --positiveRate 0.33  --positiveIndex '0' --local_epochs 3 --P_Index_accordance --randomIndex_num 2 --num_clients 50 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
 
 # baseline 2
-#python main.py  --positiveRate 0.999  --positiveIndex '0' --local_epochs 3 --P_Index_accordance --randomIndex_num 2 --num_clients 50 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
+python main.py  --positiveRate 0.999  --positiveIndex '0' --local_epochs 3 --P_Index_accordance --randomIndex_num 2 --num_clients 50 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
 
 
