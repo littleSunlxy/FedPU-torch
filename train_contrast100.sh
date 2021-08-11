60c587731e0d501a5e6deefcb6fefcb16b0b42ac
linxinyang <linxinyang@meituan.com>
2021-08-11 16:32:43 +0800

    config setting

--- train_contrast100.sh
+++ train_contrast100.sh
@@ -1,2 +1,2 @@
 # client 100 contrast
-python main.py  --dataset 'CIFAR10' --pu_batchsize 256 --positiveRate 0.1  --local_epochs 2 --P_Index_accordance --randomIndex_num 10 --num_clients 100 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
+python main.py  --dataset 'CIFAR10' --pu_batchsize 128 --positiveRate 0.1  --local_epochs 1 --P_Index_accordance --randomIndex_num 10 --num_clients 100 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.1
