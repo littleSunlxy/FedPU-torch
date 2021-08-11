c4a078d0ebfa9e9fd64100cc359d7b19c3e14952
linxinyang <linxinyang@meituan.com>
2021-08-11 13:22:21 +0800

    config setting

--- train10.sh
+++ train10.sh
@@ -5,4 +5,4 @@
 # client 50
 #python main.py  --positiveRate 0.01 --randomIndex_num 2 --num_clients 100 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.05
 # client 20
-python main.py  --positiveRate 0.33 --P_Index_accordance --randomIndex_num 2 --num_clients 10 --communication_rounds 2000 --classes_per_client 10 --clientSelect_Rate 0.2
+python main.py  --positiveRate 0.33 --P_Index_accordance --randomIndex_num 2 --num_clients 10 --communication_rounds 500 --classes_per_client 10 --clientSelect_Rate 0.2
