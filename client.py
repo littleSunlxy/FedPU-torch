4d0f341b521b1a0006419fa80d2bc1bf01093b95
linxinyang <linxinyang@meituan.com>
2021-08-17 19:32:25 +0800

    config setting

--- client.py
+++ client.py
@@ -66,7 +66,7 @@ class Client:
                     loss = self.loss(outputs, labels)
                 if opt.positiveIndex == 'randomIndexList':
                     loss, ploss, uloss = self.loss(outputs, labels, self.priorlist, self.indexlist)
-                print("lr:", self.optimizer_pu.param_groups[-1]['lr'])
+                # print("lr:", self.optimizer_pu.param_groups[-1]['lr'])
                 loss.backward()
                 # if i == 0:
                 #     print("epoch", epoch, "loss:", loss, "ploss", ploss, "uloss", uloss)
