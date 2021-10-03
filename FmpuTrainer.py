81c8d8635312fb3b0baf950fb0168b2326e1f03b
linxinyang <810427220@qq.com>
2021-10-03 16:34:32 +0800

    add fedmatch dataloader

--- FmpuTrainer.py
+++ FmpuTrainer.py
@@ -106,7 +106,6 @@ class FmpuTrainer:
 
     def clients_train_step(self):
         for idx in self.clientSelect_idxs:
-            print("client:",idx)
             self.clients[idx].train_pu()
 
     def clients_train_step_P(self):
