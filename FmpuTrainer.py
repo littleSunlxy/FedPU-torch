933ee2c2b7fcf377a8ab2f371a6fc78ef29c8244
linxinyang <810427220@qq.com>
2021-10-03 16:14:57 +0800

    add fedmatch dataloader

--- FmpuTrainer.py
+++ FmpuTrainer.py
@@ -34,7 +34,7 @@ class FmpuTrainer:
             _, transforms_eval = get_default_data_transforms(opt.dataset, verbose=False)
             test_dataset = CustomImageDataset(self.x_test, self.y_test, transforms_eval)
             test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=opt.test_batchsize, shuffle=True)
-            import pdb; pdb.set_trace()
+
             self.clients = [Client(_id + 1, copy.deepcopy(model_pu).cuda(), indexlist=indexList)
                             for _id, indexList, in zip(list(range(opt.num_clients)), indexlist)]
             print("numclients:", opt.num_clients, "build clients:", len(self.clients))
