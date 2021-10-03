149cc1954077a5e53baca3aced0d661ea9299ae4
linxinyang <810427220@qq.com>
2021-10-03 21:01:48 +0800

    add fedmatch dataloader

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 # debug Fedmatch dataloader
 
-python main.py   --dataset 'CIFAR10'  --useFedmatchDataLoader  --task = 'lc-biid-c10' --method 'FedAVG-SL-iid' --num_clients 100 --local_epochs 10 --communication_rounds 400 --pu_lr 0.01 --clientSelect_Rate 0.05
+python main.py   --dataset 'CIFAR10'  --useFedmatchDataLoader  --task 'lc-biid-c10' --method 'FedAVG-SL-iid' --num_clients 100 --local_epochs 10 --communication_rounds 400 --pu_lr 0.01 --clientSelect_Rate 0.05
