c46cccef917e258c71942e06105cfef690e21d7b
linxinyang <810427220@qq.com>
2021-10-03 16:29:40 +0800

    add fedmatch dataloader

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 # debug Fedmatch dataloader
 
-python main.py   --dataset 'CIFAR10'  --useFedmatchDataLoader --positiveRate 0.01 --P_Index_accordance --randomIndex_num 2 --num_clients 100 --communication_rounds 200 --classes_per_client 10 --clientSelect_Rate 0.1
+python main.py   --dataset 'CIFAR10'  --useFedmatchDataLoader  --num_clients 100 --communication_rounds 400 --clientSelect_Rate 0.05
