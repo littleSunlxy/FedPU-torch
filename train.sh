7431104fa208a36ba082b23cb875d801ed6e707d
linxinyang <810427220@qq.com>
2021-10-08 16:16:46 +0800

    add fedmatch dataloader

--- train.sh
+++ train.sh
@@ -13,4 +13,4 @@
 #python main.py   --dataset 'CIFAR10'  --useFedmatchDataLoader  --task 'lc-bimb-c10' --method 'FedPU' --num_clients 100 --local_epochs 1 --communication_rounds 400 --pu_lr 0.01 --clientSelect_Rate 0.05
 
 
-python main.py   --dataset 'CIFAR10' --method 'FedPU' --num_clients 1 --pu_batchsize 2048 --classes_per_client 10 --P_Index_accordance --positiveRate 0.5 --randomIndex_num 10 --local_epochs 1 --communication_rounds 5 --pu_lr 0.01 --clientSelect_Rate 1.0
+python main.py   --dataset 'CIFAR10' --method 'FedPU' --num_clients 1 --pu_batchsize 2048 --classes_per_client 10 --P_Index_accordance --positiveRate 0.5 --randomIndex_num 10 --local_epochs 5 --communication_rounds 5 --pu_lr 0.01 --clientSelect_Rate 1.0
