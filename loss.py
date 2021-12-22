2d3578dc919e75e5d3a182ff0b6522d420c5b2fb
linxinyang <linxinyang@meituan.com>
2021-12-22 21:47:35 +0800

    config setting

--- loss.py
+++ loss.py
@@ -110,3 +110,53 @@ class MPULoss_INDEX(nn.Module):
         objective = PULoss * self.puW + crossloss
 
         return objective, PULoss * self.puW, crossloss
+
+
+class MPULoss_V2(nn.Module):
+    def __init__(self, k, puW):
+        super().__init__()
+        self.numClass = torch.tensor(k).cuda()
+        self.puW = puW
+
+    def forward(self, outputs, labels, priorlist, indexlist):
+        outputs = outputs.float()
+        outputs_Soft = F.softmax(outputs, dim=1)
+        # 数据划分
+        P_mask = (labels <= self.numClass - 1).nonzero(as_tuple=False).view(-1).cuda()
+        labelsP = torch.index_select(labels, 0, P_mask)
+        outputsP = torch.index_select(outputs, 0, P_mask)
+        outputsP_Soft = torch.index_select(outputs_Soft, 0, P_mask)
+        # import pdb; pdb.set_trace()
+
+
+        U_mask = (labels > self.numClass - 1).nonzero(as_tuple=False).view(-1).cuda()
+        outputsU = torch.index_select(outputs, 0, U_mask)               #  unlabeldata 的 ground truth. setting限制不能使用
+        outputsU_Soft = torch.index_select(outputs_Soft, 0, U_mask)     #  所有在 unlabeldata 上的预测值
+
+        PULoss = torch.zeros(1).cuda()
+
+        for i in range(self.numClass):
+            if i in indexlist:      # calculate ui
+                pu3 = sum(-torch.log(1 - outputsU_Soft[:, i] + 0.01)) / \
+                      max(1, outputsU.size(0)) / len(indexlist)
+                PULoss += pu3
+            else:
+                pu1 = sum(-torch.log(1 - outputsP_Soft[:, i] + 0.01)) * \
+                      priorlist[indexlist[0]] / max(1, outputsP.size(0)) / (self.numClass-len(indexlist))
+                PULoss += pu1
+
+
+
+        pu2 = torch.zeros(1).cuda()
+        for index, i in enumerate(labelsP):   # need to be optimized
+            x = outputsP_Soft[index][i]
+            pu2 += -torch.log(1 - x + 0.01) * priorlist[i]
+
+        PULoss -= pu2 / max(1, outputsP.size(0))
+
+        crossentropyloss=nn.CrossEntropyLoss()
+        crossloss = crossentropyloss(outputsP, labelsP)
+
+        objective = PULoss * self.puW + crossloss
+
+        return objective, PULoss * self.puW, crossloss
