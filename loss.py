af87e10a8415025be198b20707e45710b4a753e8
linxinyang <810427220@qq.com>
2021-12-23 11:41:21 +0800

    add fedmatch dataloader

--- loss.py
+++ loss.py
@@ -125,7 +125,8 @@ class MPULoss_V2(nn.Module):
         outputs_Soft = F.softmax(outputs, dim=1)
         new_P_indexlist =  torch.zeros(self.numClass).cuda()
         # import pdb; pdb.set_trace()
-        for i in indexlist.long():
+        indexlist = indexlist.long()
+        for i in indexlist():
             new_P_indexlist[i] += 1
         # 数据划分
         P_mask = (labels <= self.numClass - 1).nonzero(as_tuple=False).view(-1).cuda()
