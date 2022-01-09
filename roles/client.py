382f679754ae6c895a45d50487f9a01c04854e27
linxinyang <linxinyang@meituan.com>
2022-01-10 00:13:07 +0800

    config setting

--- roles/client.py
+++ roles/client.py
@@ -144,7 +144,7 @@ class Client:
                 # iterate through the current and global model parameters
                 import pdb;
                 pdb.set_trace()
-                if globalmodel.items() == 0:
+                if len(globalmodel.items()) == 0:
                     globalmodel = self.model
                 for w, w_t in zip(self.model.state_dict().items(), globalmodel.state_dict().items()):
                     # update the proximal term
