537d9f63c115cd7c1d9bd04b742a0c41b28b9a6d
linxinyang <linxinyang@meituan.com>
2022-01-10 17:53:44 +0800

    config setting

--- train_fedProx.sh
+++ train_fedProx.sh
@@ -2,14 +2,14 @@
 
 # exp in original paper
 #01.log
-#python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.0 --percentage 0.0 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
+python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.0 --percentage 0.0 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 #02.log
 #python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.0 --percentage 0.5 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 #03.log
 #python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.0 --percentage 0.9 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 
 #04.log
-python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.01 --percentage 0.0 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
+#python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.01 --percentage 0.0 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 #05.log
 #python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.01 --percentage 0.5 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 #06.log
