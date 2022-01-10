334c4dfcc50c8a244042a2f6eab78bf9e458a9c8
linxinyang <linxinyang@meituan.com>
2022-01-10 13:42:49 +0800

    config setting

--- train_fedProx.sh
+++ train_fedProx.sh
@@ -1,7 +1,7 @@
 # train FedProx
 
 # exp in original paper
-#python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.0 --percentage 0.0 --num_clients 100 --pu_batchsize 2048 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
+python main.py   --dataset 'CIFAR10' --method 'FedProx' --mu 0.0 --percentage 0.0 --num_clients 100 --pu_batchsize 128 --classes_per_client 10 --P_Index_accordance --positiveRate 0.99 --randomIndex_num 10 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 
-# exp for FedPU
-python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0.0 --percentage 0.0 --num_clients 100 --pu_batchsize 2048 --classes_per_client 10 --P_Index_accordance --positiveRate 0.5 --randomIndex_num 10 --communication_rounds 50 --pu_lr 0.01 --clientSelect_Rate 0.1
+## exp for FedPU
+#python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0.0 --percentage 0.0 --num_clients 100 --pu_batchsize 2048 --classes_per_client 10 --P_Index_accordance --positiveRate 0.5 --randomIndex_num 10 --communication_rounds 50 --pu_lr 0.01 --clientSelect_Rate 0.1
