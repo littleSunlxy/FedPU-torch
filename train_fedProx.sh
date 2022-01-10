e0037a7c3359d6f0827117daeb6e1a0918f5ddd4
linxinyang <linxinyang@meituan.com>
2022-01-11 00:47:07 +0800

    config setting

--- train_fedProx.sh
+++ train_fedProx.sh
@@ -18,6 +18,6 @@
 #04.log
 #python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0 --percentage 0.0 --num_clients 10 --pu_batchsize 2048 --classes_per_client 2 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 ##05.log
-python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0 --percentage 0.5 --num_clients 10 --pu_batchsize 2048 --classes_per_client 2 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
+#python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0 --percentage 0.5 --num_clients 10 --pu_batchsize 2048 --classes_per_client 2 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
 ##06.log
-#python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0 --percentage 0.9 --num_clients 10 --pu_batchsize 2048 --classes_per_client 2 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
+python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0 --percentage 0.9 --num_clients 10 --pu_batchsize 2048 --classes_per_client 2 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.1
