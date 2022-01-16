5849b7db8835a2519f666b5bfb5c54b1469cbdcd
linxinyang <linxinyang@meituan.com>
2022-01-16 14:41:30 +0800

    config setting

--- train_fedProx.sh
+++ train_fedProx.sh
@@ -2,7 +2,7 @@
 
 # exp in original paper
 #01.log
-python main.py  --useFedmatchDataLoader  --dataset 'CIFAR10' --method 'FedPU' --usePU --FedProx_Epochs 2 --mu 0.10 --percentage 0.0 --num_clients 100 --pu_batchsize 512 --communication_rounds 2000 --pu_lr 0.01 --clientSelect_Rate 0.1
+python main.py  --useFedmatchDataLoader  --dataset 'CIFAR10' --method 'FedPU' --FedProx_Epochs 2 --mu 0.10 --percentage 0.0 --num_clients 100 --pu_batchsize 512 --communication_rounds 2000 --pu_lr 0.01 --clientSelect_Rate 0.1
 
 #02.log
 #python main.py  --useFedmatchDataLoader --dataset 'CIFAR10' --task 'lc-bimb-c10' --method 'FedProx' --mu 0.0 --percentage 50 --num_clients 100 --pu_batchsize 512 --classes_per_client 2 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --communication_rounds 200 --pu_lr 0.01 --clientSelect_Rate 0.05
@@ -15,7 +15,6 @@ python main.py  --useFedmatchDataLoader  --dataset 'CIFAR10' --method 'FedPU' --
 #python main.py  --useFedmatchDataLoader  --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0.01 --percentage 0.5 --num_clients 100 --pu_batchsize 512 --communication_rounds 500 --pu_lr 0.01 --clientSelect_Rate 0.1
 ##06.log
 #python main.py  --useFedmatchDataLoader  --dataset 'CIFAR10' --method 'FedProx' --usePU --mu 0.01 --percentage 0.9 --num_clients 100 --pu_batchsize 512 --communication_rounds 500 --pu_lr 0.01 --clientSelect_Rate 0.1
-
 
 #07.log
 #python main.py  --useFedmatchDataLoader --dataset 'CIFAR10'  --task 'lc-bimb-c10' --method 'FedProx' --FedProx_Epochs 1 --usePU --mu 0.01 --percentage 0.0 --num_clients 100 --pu_batchsize 512 --communication_rounds 500 --pu_lr 0.01 --clientSelect_Rate 0.1
