a6834663116dfeccdfe04518d582dd998f9f81a5
linxinyang <linxinyang@meituan.com>
2022-01-17 20:01:28 +0800

    config setting

--- modules/loss.py
+++ modules/loss.py
@@ -161,7 +161,7 @@ class MPULoss_V2(nn.Module):
         crossloss = crossentropyloss(outputsP, labelsP)
 
         # objective = PULoss * self.puW
-        objective = crossloss
-        # objective = PULoss * self.puW + crossloss
+        # objective = crossloss
+        objective = PULoss * self.puW + crossloss
 
         return objective, PULoss * self.puW, crossloss
