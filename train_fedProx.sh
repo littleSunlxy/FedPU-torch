61c53b41e259eb479e52f47526cb8b9904cdfac0
linxinyang <linxinyang@meituan.com>
2022-01-22 15:59:24 +0800

    config setting

--- train_fedProx.sh
+++ train_fedProx.sh
@@ -15,5 +15,5 @@
 
 
 # use my data spilt setting
-python main.py   --dataset 'CIFAR10' --method 'FedProx_SL' --adjust_lr --local_epochs 20 --communication_rounds 500 --pu_weight 1 --mu 0.1 --percentage 50 --num_clients 10 --pu_batchsize 512 --classes_per_client 5 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --pu_lr 0.01 --clientSelect_Rate 0.2
+python main.py   --dataset 'CIFAR10' --method 'FedProx' --usePU --adjust_lr --local_epochs 20 --communication_rounds 500 --pu_weight 0 --mu 0.1 --percentage 0 --num_clients 10 --pu_batchsize 512 --classes_per_client 5 --P_Index_accordance --positiveRate 0.33 --randomIndex_num 2 --pu_lr 0.01 --clientSelect_Rate 0.2
 
