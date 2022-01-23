b68b34c45986d703fa609c170820c501b22c1bef
linxinyang <linxinyang@meituan.com>
2022-01-23 15:39:53 +0800

    config setting

--- roles/FmpuTrainer.py
+++ roles/FmpuTrainer.py
@@ -95,7 +95,6 @@ class FmpuTrainer:
                     self.clients[idx].train_fedprox_pu(epochs=heterogenous_epoch_list[idx], mu=mu,
                                                        globalmodel=self.cloud.aggregated_client_model)
                 else:
-                    import pdb; pdb.set_trace()
                     self.clients[idx].train_fedprox_p(epochs=heterogenous_epoch_list[idx], mu=mu,
                                                        globalmodel=self.cloud.aggregated_client_model)
         elif 'FedPU' in opt.method:
@@ -116,6 +115,8 @@ class FmpuTrainer:
             heterogenous_epoch_list = np.array(heterogenous_epoch_list)
             for idx in self.clientSelect_idxs:
                 self.clients[idx].model.load_state_dict(self.cloud_lastmodel.state_dict())
+                import pdb;
+                pdb.set_trace()
                 self.clients[idx].train_fedprox_p(epochs=heterogenous_epoch_list[idx], mu=mu,
                                                   globalmodel=self.cloud.aggregated_client_model)
         elif 'FedPU' in opt.method:
